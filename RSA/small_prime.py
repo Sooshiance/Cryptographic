0c3af7917c7f54435ce51c8616469b5a0802bda6
sooshiance <s.katafract@gmail.com>
2024-08-26 17:11:33 +0330

    build: Basic operations of RSA

---
+++ RSA/small_prime.py
@@ -0,0 +1,23 @@
+import math
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def mod_inverse(a, m):
+    m0, x0, x1 = m, 0, 1
+    while a > 1:
+        q = a // m
+        m, a = a % m, m
+        x0, x1 = x1 - q * x0, x0
+    return x1 + m0 if x1 < 0 else x1
