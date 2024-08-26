0cf9106def611ac1a97ecc49cfa6b5a19e871a97
sooshiance <s.katafract@gmail.com>
2024-08-27 00:06:40 +0330

    build: Create Process block and make ready for last step

--- Hash/sha_one.cc
+++ Hash/sha_one.cc
@@ -64,3 +64,64 @@ std::string SHA1::final()
     result << std::setw(8) << h0 << std::setw(8) << h1 << std::setw(8) << h2 << std::setw(8) << h3 << std::setw(8) << h4;
     return result.str();
 }
+
+void SHA1::processBlock(const uint8_t *block)
+{
+    uint32_t w[80];
+    for (int i = 0; i < 16; ++i)
+    {
+        w[i] = (block[i * 4] << 24) | (block[i * 4 + 1] << 16) | (block[i * 4 + 2] << 8) | block[i * 4 + 3];
+    }
+    for (int i = 16; i < 80; ++i)
+    {
+        w[i] = (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]);
+        w[i] = (w[i] << 1) | (w[i] >> 31);
+    }
+
+    uint32_t a = h0, b = h1, c = h2, d = h3, e = h4;
+    for (int i = 0; i < 80; ++i)
+    {
+        uint32_t f, k;
+        if (i < 20)
+        {
+            f = (b & c) | ((~b) & d);
+            k = 0x5A827999;
+        }
+        else if (i < 40)
+        {
+            f = b ^ c ^ d;
+            k = 0x6ED9EBA1;
+        }
+        else if (i < 60)
+        {
+            f = (b & c) | (b & d) | (c & d);
+            k = 0x8F1BBCDC;
+        }
+        else
+        {
+            f = b ^ c ^ d;
+            k = 0xCA62C1D6;
+        }
+        uint32_t temp = ((a << 5) | (a >> 27)) + f + e + k + w[i];
+        e = d;
+        d = c;
+        c = (b << 30) | (b >> 2);
+        b = a;
+        a = temp;
+    }
+
+    h0 += a;
+    h1 += b;
+    h2 += c;
+    h3 += d;
+    h4 += e;
+}
+
+void SHA1::processBuffer()
+{
+    while (buffer.size() >= 64)
+    {
+        processBlock(buffer.data());
+        buffer.erase(buffer.begin(), buffer.begin() + 64);
+    }
+}
